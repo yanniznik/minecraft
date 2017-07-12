cb70e56ae6d3a4c5ffa42cd753773739d61dd621
yanniznik <account@yananas.com>
2017-07-12 14:36:39 +0300

    Good version

--- one.js
+++ one.js
@@ -7,7 +7,7 @@
 		test.generateMap(1000);
 		test.generateBlock();
 		Minecraft.elements( Minecraft.random(2,Minecraft.oneLine),ground);
-		Minecraft.initializeTools(Minecraft.toolName, Minecraft.blocks);
+		Minecraft.initializeTools();
 	}
 
 	Minecraft.random = function(min, max, excluded) {
@@ -16,58 +16,48 @@
 		return n;
 	}
 
-	Minecraft.block = function(type, x, y) {
+	Minecraft.selectedTool = [];
 
+	Minecraft.tools = function (type, approachedBlock){
+		this.type = type;
+		this.approachedBlock = approachedBlock;
+		var self = this;
+		this.toolHolder = $("<div>", {"class": "tool " + this.type, "style": "background-image: url('images/" + this.type + ".png')"});
+		$(".tools-container").append(this.toolHolder);
+		this.toolHolder.click(function(){
+			Minecraft.selectedTool = [];
+			Minecraft.selectedTool = [self.type, self.approachedBlock];
+			 $(".tool").removeClass('active');
+  $(this).addClass('active');
+		})
+	}
+
+	Minecraft.initializeTools = function () {
+		new Minecraft.tools("axe", ["tree", "leaf"]);
+		new Minecraft.tools("shovel", ["dirt", "grass"]);
+		new Minecraft.tools("pickaxe", ["rock"]);
+		new Minecraft.tools("eraser", ["tree", "leaf", "dirt", "grass", "rock", "cloud"]);
+	}
+
+	Minecraft.block = function(type, x, y) {
 		this.type = type;
 		this.coordinates = [x, y];
+		var self = this;
 		this.blockHolder = $("<div>", {"class": "block " + this.type, "style": "background-image: url('images/" + this.type + ".png')"});
 		$("#map").append(this.blockHolder);
 		this.changeType = function(type) {
 			this.type = type;
 			this.blockHolder.css("background-image","url('images/" + this.type + ".png')");
 		}
+		this.blockHolder.click(function() {
+			for (var i = 0; i < Minecraft.selectedTool[1].length; i++) {
+				targetBlock = Minecraft.selectedTool[1][i];
+				if (targetBlock == self.type) {
+					self.changeType("blank");
+				}
+			}
+		})
 	}
-
-	Minecraft.block.prototype.create = function (){
- 
- var self = this;
- /*this.domBlock = document.createElement("div");
-   //Each DOM block element will have class block 
-   this.domBlock.classList.add("block", "dirt");
-   //not all background images have a png extension, use if/else based on type
-   this.domBlock.style.backgroundImage =  "url('./images/" + this.type + ".png')"*/
-   //document.getElementById("placeholder").appendChild(this.$blockHolder);
-
-   console.log(self);
-
-   this.blockHolder.click(function () {
-	   console.log('block clicked');
-     console.log(self);
-     if(Minecraft.selectedTool != null){
-      Minecraft.block.prototype.tryToRemove(self);
-    }
-  })
- }
-
- Minecraft.block.prototype.tryToRemove = function (block){
-  console.log('in delete function');
-  console.log(block.blockHolder)
-    //if the block objects DOM element contains a class that matches the Selecetedtools
-    //approachedblock, then the tool and the block match, and we can remove the block
-
-    if(block.blockHolder.hasClass(Minecraft.selectedTool.approachedBlock)){
-    //types need to match classes, whatever object we create in the constructor
-    //their classes should be set to type
-	console.log('Y')
-    block.blockHolder.removeClass(block.type);
-    block.blockHolder.addClass("sky");
-    //console.log(block.domBlock.classList);
-    
-  }
-
-}
-
- 
 
 	Minecraft.map = function() {
 		this.x = 1;
@@ -86,9 +76,6 @@
 					this.x = 1;
 				}
 				Minecraft.allBlocks[(Minecraft.f+1)] = new Minecraft.block(block, this.x, this.y);
-				console.log(Minecraft.allBlocks[(Minecraft.f+1)]);
-				Minecraft.allBlocks[(Minecraft.f+1)].create(); 
-
 				Minecraft.f++;
 			}
 			if (block == "grass") {
@@ -142,50 +129,7 @@
 		}
 	} // end elements
 
-	Minecraft.tools = function (type, approachedBlock){
-
-		this.toolHolder;
-		this.type = type;
-		this.approachedBlock = approachedBlock;
-		this.selected = false;
-
-		this.create = function() {
-
-			var self = this;
-			this.toolHolder = document.createElement("div");
-			this.toolHolder.classList.add("tool");
-			this.toolHolder.style.backgroundImage =  "url('./images/" + this.type + ".png')";
-			$(".tools-container").append(this.toolHolder);
-
-			this.toolHolder.addEventListener('click', function () {
-				Minecraft.selectTool(self);
-			})
-
-		}
-}
-
-Minecraft.allTools = [];
-Minecraft.allBlocks = [];
-Minecraft.selectedTool;
-Minecraft.selectedBlock;
-Minecraft.toolName = ["pickaxe","shovel","axe"];
-Minecraft.blocks = ["rock", "dirt", "wood", "sky", "cloud", "leaves"];
-
-Minecraft.initializeTools = function (tools, blocks) {
-
-	for(var i = 0; i < tools.length; i++){
-		Minecraft.allTools[i] = new Minecraft.tools(tools[i], blocks[i]);
-		console.log(tools, blocks);
-		Minecraft.allTools[i].create();
-	}
-}
-
-Minecraft.selectTool = function (self){
-//better approach then setting every object type false
-Minecraft.selectedTool = self;
-
-console.log(Minecraft.selectedTool);
-}
 
 
-Minecraft.start();
+
+	Minecraft.start();
